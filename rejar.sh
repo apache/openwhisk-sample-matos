e99353113def827403bc1ed0659878ac6ab26c92
glikson <glikson@il.ibm.com>
2016-09-11 11:59:23 -0400

    Updated rejar script to match the new repository name
    The names of jar produced by gradle are based on repository name

--- rejar.sh
+++ rejar.sh
@@ -4,13 +4,13 @@ gradle jarMonitor jarLoader jarBatch
 
 echo -n Repackaging jars...
 
-rm -rf tmp/* run/matos-load.jar ; cd tmp ; jar xf ../build/libs/matos-load-1.0.jar ; jar cfe ../run/matos-load.jar com.ibm.matos.Load com/ibm/matos/Load.class *; cd ..
+rm -rf tmp/* run/matos-load.jar ; cd tmp ; jar xf ../build/libs/openwhisk-sample-matos-load-1.0.jar ; jar cfe ../run/matos-load.jar com.ibm.matos.Load com/ibm/matos/Load.class *; cd ..
 echo -n Load..
 
-rm -rf tmp/* run/matos-batch.jar ; cd tmp ; jar xf ../build/libs/matos-batch-1.0.jar ; jar cfe ../run/matos-batch.jar com.ibm.matos.Batch com/ibm/matos/Batch.class *; cd ..
+rm -rf tmp/* run/matos-batch.jar ; cd tmp ; jar xf ../build/libs/openwhisk-sample-matos-batch-1.0.jar ; jar cfe ../run/matos-batch.jar com.ibm.matos.Batch com/ibm/matos/Batch.class *; cd ..
 echo -n Batch..
 
-rm -rf tmp/* run/matos-monitor.jar ; cd tmp ; jar xf ../build/libs/matos-monitor-1.0.jar ; jar cfe ../run/matos-monitor.jar com.ibm.matos.Monitor com/ibm/matos/Monitor.class *; cd ..
+rm -rf tmp/* run/matos-monitor.jar ; cd tmp ; jar xf ../build/libs/openwhisk-sample-matos-monitor-1.0.jar ; jar cfe ../run/matos-monitor.jar com.ibm.matos.Monitor com/ibm/matos/Monitor.class *; cd ..
 echo Monitor
 
 echo Done
