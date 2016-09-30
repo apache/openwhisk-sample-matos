40e60bd16deb8e3da770e9d0d7548b2dadcebf0c
glikson <glikson@il.ibm.com>
2016-09-30 16:51:12 -0400

    Combine "create" and "update" in the same script

--- rewhisk.sh
+++ rewhisk.sh
@@ -1,8 +1,24 @@
 #/bin/sh
 
-echo Updating Whisk...
-wsk action update matos/load run/matos-load.jar
-wsk action update matos/batch run/matos-batch.jar
-wsk action update matos/monitor run/matos-monitor.jar
+whisk_create_or_update () {
 
+    ACTION=$1
+    FILE=$2
+    wsk action get -s $ACTION 2> /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo Updating action $ACTION referring to $FILE
+        wsk action update $ACTION $FILE
+    else
+        echo Creating action $ACTION referring to $FILE
+        wsk action create $ACTION $FILE
+    fi
+}
+
+echo Updating Whisk...
+whisk_create_or_update matos/load run/matos-load.jar
+whisk_create_or_update matos/batch run/matos-batch.jar
+whisk_create_or_update matos/monitor run/matos-monitor.jar
+whisk_create_or_update matos/batchW js/batchW.js
 echo Done
+
